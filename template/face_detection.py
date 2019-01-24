08eb20c731f67caa15638f7ce31032cc2a5d57dc
Josh S <joshshigaki@users.noreply.github.com>
2019-01-24 15:53:54 -0800

    file upload/ model load / building on jupyter

---
+++ template/face_detection.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+from keras.models import load_model
+from statistics import mode
+from utils.datasets import get_labels
+from utils.inference import detect_faces
+from utils.inference import draw_text
+from utils.inference import draw_bounding_box
+from utils.inference import apply_offsets
+from utils.inference import load_detection_model
+from utils.preprocessor import preprocess_input
+
+USE_WEBCAM = True # If false, loads video file source
+
+# parameters for loading data and images
+emotion_model_path = './models/emotion_model.hdf5'
+emotion_labels = get_labels('fer2013')
+
+# hyper-parameters for bounding boxes shape
+frame_window = 10
+emotion_offsets = (20, 40)
+
+# loading models
+face_cascade = cv2.CascadeClassifier('./models/haarcascade_frontalface_default.xml')
+emotion_classifier = load_model(emotion_model_path)
+
+# getting input model shapes for inference
+emotion_target_size = emotion_classifier.input_shape[1:3]
+
+# starting lists for calculating modes
+emotion_window = []
+
